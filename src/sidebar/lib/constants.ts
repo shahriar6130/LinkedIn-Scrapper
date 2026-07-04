19e253e85e8ed2ba030a42e960a6149183bfd700
shahriar-2023216004@cs.du.ac.bd <shahriar-2023216004@cs.du.ac.bd>
2026-07-04 15:43:10 +0600

    Remove Legacy Code & Standardize Project II

--- src/sidebar/lib/constants.ts
+++ src/sidebar/lib/constants.ts
@@ -1,33 +1,3 @@
 export const SIDEBAR_WIDTH = 380;
 export const SIDEBAR_COLLAPSED_WIDTH = 52;
 export const STORAGE_KEY = "alumni_leads";
-export const SIDEBAR_WIDTH = 380;
-export const SIDEBAR_COLLAPSED_WIDTH = 52;
-export const STORAGE_KEY = "alumni_leads";
-
-export interface Alumni {
-  name: string;
-  designation: string;
-  companyName: string;
-  profileLink: string;
-  location: string;
-  educationInstitute: string;
-  degree: string;
-  educationTimeline: string;
-  profilePicture: string;
-  connectionDegree: string;
-  addedAt: number;
-}
-
-export interface ProfileData {
-  name: string;
-  designation: string;
-  companyName: string;
-  profileLink: string;
-  location: string;
-  educationInstitute: string;
-  degree: string;
-  educationTimeline: string;
-  profilePicture: string;
-  connectionDegree: string;
-}
